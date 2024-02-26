966de351e6d1e0769d2f0bd0fe55fad6b4e1461e
Hemant Kakkar <hemant@administrators-MacBook-Pro.local>
2024-02-26 16:02:17 +0530

    Adding info box for goals

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
 import CourseGoal from "./CourseGoal";
 import { CourseGoal as CGoal } from "../App"
+import InfoBox from "./InfoBox";
+import { ReactNode } from "react";
 
 type CourseGoalListProps = {
     goals: CGoal[],
@@ -7,7 +9,17 @@ type CourseGoalListProps = {
 }
 
 export default function CourseGoalList ({goals, onDeleteGoal}: CourseGoalListProps) {
+    if(goals.length === 0) {
+        return (<InfoBox mode="hint"> You have no goals yet</InfoBox>)
+    }
+    let warningBox: ReactNode
+
+    if(goals.length>=4){
+        warningBox = <InfoBox mode="warning" severity="medium">You have lot of goals, Don't put too much on your plate</InfoBox>
+    }
     return (
+        <>
+        {warningBox}
         <ul>
             {goals.map((goal) => (
                 <li key={goal.id}>
@@ -20,5 +32,6 @@ export default function CourseGoalList ({goals, onDeleteGoal}: CourseGoalListPro
                 </li>
             ))}
         </ul>
+        </>
     )
 }
